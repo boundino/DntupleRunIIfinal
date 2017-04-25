982ee4e4b81aa2b8759dc86795a8ac7fe0abcb5b
Jing Wang <boundino@gmail.com>
2017-04-25 21:31:41 +0200

    fix bug in combineGJTO

--- CrossSection/combineGJTO.C
+++ CrossSection/combineGJTO.C
@@ -19,19 +19,55 @@ void combineGJTO(TString inputGJ, TString inputTO, TString outputplot, double lu
   
   TGraphAsymmErrors* gaeRatioCrossFONLLstat = (TGraphAsymmErrors*)fGJ->Get("gaeRatioCrossFONLLstat");
   TGraphAsymmErrors* gaeRatioCrossFONLLsyst = (TGraphAsymmErrors*)fGJ->Get("gaeRatioCrossFONLLsyst");
-  TGraphAsymmErrors* gaeCrossSyst = (TGraphAsymmErrors*)fGJ->Get("gaeCrossSyst");
   TGraphAsymmErrors* gaeBplusReference = (TGraphAsymmErrors*)fGJ->Get("gaeSigmaDzero");
-  TGraphAsymmErrors* gaeRatioCrossFONLLunity = (TGraphAsymmErrors*)fGJ->Get("gaeRatioCrossFONLLunity");
   TH1D* hfPrompt = (TH1D*)fGJ->Get("hfprompt");
 
+  TGraphAsymmErrors* gaeCrossSystGJ = (TGraphAsymmErrors*)fGJ->Get("gaeCrossSyst");
+  gaeCrossSystGJ->SetName("gaeCrossSystGJ");
+  TGraphAsymmErrors* gaeRatioCrossFONLLunityGJ = (TGraphAsymmErrors*)fGJ->Get("gaeRatioCrossFONLLunity");
+  gaeRatioCrossFONLLunityGJ->SetName("gaeRatioCrossFONLLunityGJ");
+  TGraphAsymmErrors* gaeCrossSystTO = (TGraphAsymmErrors*)fTO->Get("gaeCrossSyst");
+  gaeCrossSystTO->SetName("gaeCrossSystTO");
+  TGraphAsymmErrors* gaeRatioCrossFONLLunityTO = (TGraphAsymmErrors*)fTO->Get("gaeRatioCrossFONLLunity");
+  gaeRatioCrossFONLLunityTO->SetName("gaeRatioCrossFONLLunityTO");
+
+  TGraphAsymmErrors* gaeCrossSyst = (TGraphAsymmErrors*)gaeCrossSystGJ->Clone("gaeCrossSyst");
+  TGraphAsymmErrors* gaeRatioCrossFONLLunity = (TGraphAsymmErrors*)gaeRatioCrossFONLLunityGJ->Clone("gaeRatioCrossFONLLunity");
+  Int_t ngrbins = gaeCrossSyst->GetN();
+  for(int i=0;i<ngrbins;i++)
+    {
+      Double_t xGJ,yGJ,xTO,yTO;
+      gaeCrossSystGJ->GetPoint(i,xGJ,yGJ);
+      gaeCrossSystTO->GetPoint(i,xTO,yTO);
+      Double_t x = xGJ;
+      Double_t y = (yGJ*lumiGJ+yTO*lumiTO)/(lumiGJ+lumiTO);
+      gaeCrossSyst->SetPoint(i,x,y);
+      Double_t exl = gaeCrossSystGJ->GetErrorXlow(i);
+      Double_t exh = gaeCrossSystGJ->GetErrorXhigh(i);
+      Double_t eyl = (gaeCrossSystGJ->GetErrorYlow(i)*lumiGJ+gaeCrossSystTO->GetErrorYlow(i)*lumiTO)/(lumiGJ+lumiTO);
+      Double_t eyh = (gaeCrossSystGJ->GetErrorYhigh(i)*lumiGJ+gaeCrossSystTO->GetErrorYhigh(i)*lumiTO)/(lumiGJ+lumiTO);
+      gaeCrossSyst->SetPointError(i,exl,exh,eyl,eyh);
+
+      Double_t xfGJ,yfGJ,xfTO,yfTO;
+      gaeRatioCrossFONLLunityGJ->GetPoint(i,xfGJ,yfGJ);
+      gaeRatioCrossFONLLunityTO->GetPoint(i,xfTO,yfTO);
+      Double_t xf = xfGJ;
+      Double_t yf = (yfGJ*lumiGJ+yfTO*lumiTO)/(lumiGJ+lumiTO);
+      gaeRatioCrossFONLLunity->SetPoint(i,xf,yf);
+      Double_t exfl = gaeRatioCrossFONLLunityGJ->GetErrorXlow(i);
+      Double_t exfh = gaeRatioCrossFONLLunityGJ->GetErrorXhigh(i);
+      Double_t eyfl = (gaeRatioCrossFONLLunityGJ->GetErrorYlow(i)*lumiGJ+gaeRatioCrossFONLLunityTO->GetErrorYlow(i)*lumiTO)/(lumiGJ+lumiTO);
+      Double_t eyfh = (gaeRatioCrossFONLLunityGJ->GetErrorYhigh(i)*lumiGJ+gaeRatioCrossFONLLunityTO->GetErrorYhigh(i)*lumiTO)/(lumiGJ+lumiTO);
+      gaeRatioCrossFONLLunity->SetPointError(i,exfl,exfh,eyfl,eyfh);
+    }
 
   TFile* outputfile = new TFile(outputplot.Data(),"recreate");
   outputfile->cd();
   hPtSigma->Write();
   gaeRatioCrossFONLLstat->Write();
   gaeRatioCrossFONLLsyst->Write();
-  gaeCrossSyst->Write();
   gaeBplusReference->Write();
+  gaeCrossSyst->Write();
   gaeRatioCrossFONLLunity->Write();
   hfPrompt->Write();
   outputfile->Close();
